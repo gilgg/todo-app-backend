5ac69dac84606998ef0215b65b53b12683e41678
Gil <limudgil@gmail.com>
2022-01-22 01:07:57 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -8,18 +8,18 @@ const userRouter = require("./routers/user");
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  next();
-});
+//   next();
+// });
 
 // app.use(express.static(path.join("public")));
 // app.use(express.static(path.join(__dirname, "public")));
@@ -30,7 +30,7 @@ app.use(userRouter);
 //   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 // });
 
-app.listen(5000, () => {
-  console.log("server is up on port 5000");
-});
-// app.listen(process.env.PORT || 5000);
+// app.listen(5000, () => {
+//   console.log("server is up on port 5000");
+// });
+app.listen(process.env.PORT || 5000);
