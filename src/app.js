ef6990e75b41e73a735dbc996de4e52bf33f26d8
Gil <limudgil@gmail.com>
2022-01-21 21:43:40 +0200

    change public folder location

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 require("dotenv").config();
 require("./db/mongoose");
@@ -8,11 +8,12 @@ const userRouter = require("./routers/user");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join("public")));
+// app.use(express.static(path.join("public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(todoRouter);
 app.use(userRouter);
 
-app.use((req, res, next) => {
+app.use("*", (req, res, next) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
